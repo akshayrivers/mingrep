54aa4e84e2126d61f8aee93cdd08b5f235e17168
Vinod Akshat <pumkininriver@gmail.com>
2025-05-13 23:13:56 +0530

    chapter 12 complete - Test driven development(TDD)+ std::env

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use std::env;
+
+use mingrep::Config;
+use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     // now we need to store these
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("The query is {query}");
-    println!("The file path is {file_path}")
+    // running the search query
+    if let Err(e) = mingrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
